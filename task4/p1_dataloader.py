dc36b23a570ce1510c0f45aee4e1af26872d6b81
km <958747457@qq.com>
2019-04-18 22:17:59 +0800

    0418

--- task4/p1_dataloader.py
+++ task4/p1_dataloader.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import sys
+
 from collections import Counter
 import jieba
 import numpy as np
@@ -9,6 +9,7 @@ import tensorflow.contrib.keras as kr
 '''
     工具包 begin
 '''
+import sys
 if sys.version_info[0] > 2:
     is_py3 = True
 else:
